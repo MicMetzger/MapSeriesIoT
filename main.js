b51446fa2357434649504c4699e2a06a7a3dc0df
Mikem <mime9599@colorado.edu>
2020-12-31 20:06:15 -0700

    fixed sidenav arrow

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
-function activate_navbar(x) {
-    if (x === "off") {
-        x.remove();
+function activate_navbar() {
+  x = document.getElementById("drop_button");
+  var switcher = document.getElementById("arrow_icon");
+  var open_menu = document.getElementById("Sidebar-Dropdown").getElementsByClassName("docs-select-item");
+
+  if (x.classList.contains("off")) {
+      x.classList.remove("off");
+      x.classList.add("on");
+
+      switcher.classList.remove("fa-angle-double-up");
+      switcher.classList.add("fa-angle-double-down")
+      
+      for (var i = 0; i < open_menu.length; i++) {
+          if (open_menu[i].class === "docs-select-item") {
+              open_menu[i].classList.add("active");
+          }
+      }
     }
-    var open_menu = document.getElementById("Sidebar-Dropdown").getElementsByClassName("docs-select-item");
-    for (var i = 0; i < open_menu.length; i++) {
-        if (open_menu[i].class === "docs-select-item") {
-            open_menu[i].classList.add("active");
+    else {
+      if (x.classList.contains("on")) {
+        x.classList.remove("on");
+        x.classList.add("off");
+        
+        switcher.classList.remove("fa-angle-double-down");
+        switcher.classList.add("fa-angle-double-up")
+
+        for (var i = 0; i < open_menu.length; i++) {
+            if (open_menu[i].class === "docs-select-item") {
+                open_menu[i].classList.remove("active");
+            }
         }
+      }
+
     }
+      
+    return
 }
 
 // Close the Side dropdown if the user clicks outside of division
